0511c6f074059340009926bd73dff726f820a262
Focused Meanderer <84609482+FocusedMeanderer@users.noreply.github.com>
2023-07-01 20:22:20 +0800

    Revert "Solve Exercise 10"
    This reverts commit 86cf93658b4ca8553b36d45856991ec67f85798e.

--- 09_palindromes/myPalindromes.js
+++
@@ -1,8 +0,0 @@
-const palindromes = function (string) {
-  const arrayed = string.toLowerCase().replace(/[,\.!\s]/g, '').split('');
-  
-  return (arrayed.join('') === arrayed.slice().reverse().join(''));
-};
-
-// Do not edit below this line
-module.exports = palindromes;
